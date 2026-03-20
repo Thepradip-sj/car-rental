acaa8ce07e1666afedc71d5a75fa4289695f929a
Thepradip-sj <adhikaryprdip101@gmail.com>
2026-03-20 09:59:34 +0530

    first commit

---
+++ rent-car/client/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="text-5xl text-red-500 font-bold">
+    I am pradip
+    </h1>
+  );
+}
