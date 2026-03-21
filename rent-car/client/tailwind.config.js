4be3150c5736c22d60e6bec1e932bfefa8576a92
Thepradip-sj <adhikaryprdip101@gmail.com>
2026-03-21 23:05:16 +0530

    Enhance Navbar functionality and update Tailwind CSS configuration for color themes

--- rent-car/client/tailwind.config.js
+++ rent-car/client/tailwind.config.js
@@ -1,10 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4f46e5",       // main color
+        "primary-dull": "#4338ca" // hover color
+      },
+    },
   },
   plugins: [],
 }
