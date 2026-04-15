ab39a38bb7a1aa16aebe3d405231d7382e357186
Thepradip-sj <adhikaryprdip101@gmail.com>
2026-04-15 23:29:29 +0530

    ...

--- rent-car/client/src/pages/owner/Dashboard.jsx
+++ rent-car/client/src/pages/owner/Dashboard.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {dummyDashboardData,assets} from '../../assets/assets';
+import { useEffect } from 'react';
+import Title from "../../components/Title";
+
 
 const Dashboard = () => {
-    Const [data,setData]=useState({
+    const [data,setData]=useState({
         totalCars:0,
         totalBookings:0,
         pendingBookings:0,
@@ -19,7 +23,35 @@ const Dashboard = () => {
         setData(dummyDashboardData);
     },[])
   return (
-    <div>Dashboard</div>
+    <div className="px-4 pt-10 md:px-10 flex-1">
+        <Title title="Admin Dashboard" subTitle="Monitor overall platform performance including total cars,bookings,revenue and recent activities" />
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-8 max-w-3xl">
+            {dashboardCards.map((card,index)=>{
+                <div key={index} className="flex gap-2 items-center justify-between p-4 rounded-md border border-boredrColor">
+                    <div>
+                <h1 className="text-xs text-gray-500">{card.title}</h1>
+                <p className="text-lg font-semibold">{card.value}</p>
+                </div>
+                <div className="flex items-center justify-center w-10 h-10 rounded-full bg-primary/10">
+                    <img src={card.icon} alt="" className="h-4 w-4"></img>
+                </div>
+                </div>
+            })}
+        </div>
+        <div className="flex flex-wrap items-start gap-6 mb-8 w-full">
+            <div className="p-4 md:p-6 border border-borderColor rounded-md max-w-lg w-full" >
+                <h1 className='text-lg font-medium'>Recent Bookings</h1>
+                <p className="text-gray-500">Latest customer bookings</p>
+                {data.recentBookings.map((booking,index)=>(
+                    <div>
+                        
+                    </div>
+                )
+                )}
+            </div>
+            <div></div>
+        </div>
+    </div>
   )
 }
 
